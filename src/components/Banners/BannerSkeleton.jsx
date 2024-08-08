4265faea82e24e7cd741f89ff0876474d5322afb
KanybekSydykov <limus.kana@gmail.com>
2024-08-08 14:01:02 +0600

    banner skeleton fix

--- src/components/Banners/BannerSkeleton.jsx
+++ src/components/Banners/BannerSkeleton.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 
 const BannerSkeleton = () => {
   return (
-    <Skeleton width={"100%"} aspectRatio={3/2} h={{ base: '220px', lg: '400px' }} mt={{ base: '40px', lg: '70px' }}>
-  </Skeleton>
+    <Skeleton width={"100vw"} maxW={'container.xl'} h={{ base: '220px', lg: '400px' }} mt={{ base: '40px', lg: '70px' }} />
   )
 }
 
